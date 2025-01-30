9e95f12f6a27e8935eb0a97b47488f032a1eec9d
Sagor Roy <sagorroy204@gmail.com>
2025-01-30 17:54:42 +0600

    add timezone

--- public/index.php
+++ public/index.php
@@ -4,19 +4,17 @@ use App\Base\Route;
 
 error_reporting(E_ALL);
 
-// Define paths
+date_default_timezone_set('Asia/Dhaka');
+
 define('BASE_PATH', __DIR__ . '/../');
 define('VIEWS_PATH', BASE_PATH . 'views/');
 define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
 
-// Composer autoload
 require BASE_PATH . 'vendor/autoload.php';
 
-// Load .env variables
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
-// Set error reporting based on environment
 if ($_ENV['APP_ENV'] === 'production') {
     ini_set('display_errors', 0);
     ini_set('log_errors', 1);
@@ -26,8 +24,6 @@ if ($_ENV['APP_ENV'] === 'production') {
 }
 
 
-// Include routes
 require_once BASE_PATH . 'routes/web.php';
 
-// Handle the incoming request
 Route::handleRequest();
