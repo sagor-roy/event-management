06f1faa2947c92aa0b74ac61932451849ed3a1dd
Sagor Roy <sagorroy204@gmail.com>
2025-01-30 18:04:51 +0600

    event attendee update

--- app/Controller/AttendeeController.php
+++ app/Controller/AttendeeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Base\Auth;
 use App\Base\Redirect;
 use App\Model\Attendee;
+use App\Model\Event;
 use App\Model\User;
 
 class AttendeeController extends Controller
@@ -22,13 +23,24 @@ class AttendeeController extends Controller
         $perPage = isset($_GET['limit']) ? (int)$_GET['limit'] : $defaultPerPage;
         $page = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
 
+        $eventModel = new Event;
+        $event =  $eventModel->getFirst('id', '=', $id);
+
+        if (!$event) {
+            echo "404 NOT FOUND";
+            exit;
+        }
+
         $attendeesModel = new Attendee;
         $attendees = $attendeesModel->paginate($perPage, $page, $id);
         $totalAtt = $attendeesModel->count();
         $totalPages = ceil($totalAtt / $perPage);
 
+        $eventModel = new Event;
+
         $data = [
             'status' => 'success',
+            'event' => $event,
             'data' => [
                 'attendees' => $attendees,
                 'pagination' => [
--- views/pages/events/event_attendee.php
+++ views/pages/events/event_attendee.php
@@ -11,7 +11,7 @@ ob_start();
         <div
             class="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
             <h2 class="text-title-md2 font-bold text-black dark:text-white">
-                Event: Joy Bangla Concert
+                Event: <?=$event['name']?>
             </h2>
 
             <nav>
